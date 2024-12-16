07712403dfe3babc0f1048859260d85b4a83c823
Sijie Cheng <LeslieCheng0701@outlook.com>
2024-12-16 17:38:37 +0800

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,3 +1,4 @@
+
 import os
 import json
 import argparse
@@ -23,165 +24,76 @@ def parse_args():
     parser.add_argument("--model_name", type=str, default="BLIP2")
     parser.add_argument("--device", type=str, default="0")
     parser.add_argument("--batch_size", type=int, default=1)
-
     # datasets
-    parser.add_argument("--dataset_name", type=str, default=None)
+    parser.add_argument("--dataset_name", type=str, default=None, help='Your dataset name should be one element of the list as followed : [tg, og, fg, rm_critique, rm_feedback, assistance, navigation, hp_h2m, hp_m2l, vqa]')
     parser.add_argument('--annotation_path', type=str, required=True)
     # result_path
     parser.add_argument("--answer_path", type=str, default="./tiny_answers")
-    parser.add_argument("--data_mode", type=str, default="VIDEO", help="You can chooce PICTURE or VIDEO")
+    parser.add_argument("--data_mode", type=str, default=None, help="You can chooce 'image' or 'video' type of your data")
+    parser.add_argument("--video_folder", type=str, default="/apdcephfs_cq10/share_1150325/csj/videgothink/goalstep_val_hp_video")
+    parser.add_argument("--image_folder", type=str, default=None)
+
     # parser.add_argument("--planning", action="store_true")
 
     args = parser.parse_args()
     return args
 
-
 def load_dataset(args):
-    annotation_path = args.annotation_path
-    with open(annotation_path, 'r') as f:
+    # 确保文件夹存在
+    folder_check = {
+        'image': args.image_folder,
+        'video': args.video_folder
+    }
+    folder_path = folder_check.get(args.data_mode)
+    if folder_path and not os.path.exists(str(folder_path)):
+        raise FileNotFoundError(f"{args.data_mode.capitalize()} folder '{folder_path}' does not exist.")
+    with open(args.annotation_path, 'r') as f:
         dataset = json.load(f)
+
     for i, d in enumerate(dataset):
-        if len(d['image_path']) != 0:
-            image_filename = d['image_path'][0].split('/')[-1]
-        else:
-            image_filename = ""
-            warnings.warn("No image in image dataset path", UserWarning)
-        dataset[i]['images'] = os.path.join(os.path.dirname(annotation_path), 'images', image_filename)
+        if args.data_mode == 'image':
+            image_filename = d.get('image_path', [''])[0]
+            # print(f"diffeerence : {d.get('image_path', [''])[0]} and {d.get('image_path', [''])}")
+            if not image_filename:
+                warnings.warn(f"No image found in dataset entry {i}.", UserWarning)
+            dataset[i]['images'] = os.path.join(args.image_folder, image_filename)
+        elif args.data_mode == 'video':
+            video_filename = d.get('video_path', '')
+            if not video_filename:
+                warnings.warn(f"No video found in dataset entry {i}.", UserWarning)
+            dataset[i]['video'] = os.path.join(args.video_folder, video_filename)
     return dataset
+
 
 def get_generation_args(dataset_name):
     print(f"---------function dataset name {dataset_name}------------")
-    if dataset_name == "tg":
-        return {
-            'max_new_tokens': 128,
-            'planning': False,
-            'tg': True,
-            'rm_critique': False,
-            'rm_feedback': False,
-            'hp_h2m':False
-        }
-    elif dataset_name == "rm_critique":
-        return {
-            'max_new_tokens': 128,
-            'planning': False,
-            'tg': False,
-            'rm_critique': True,
-            'rm_feedback': False,
-            'hp_h2m':False
-        }
-    elif dataset_name == "rm_feedback":
-        return {
-            'max_new_tokens': 128,
-            'planning': False,
-            'tg': False,
-            'rm_critique': False,
-            'rm_feedback': True,
-            'hp_h2m':False
-        }
-    elif dataset_name in ['assistance', 'navigation']:
-        return {
-            'max_new_tokens': 300,
-            'planning': True,
-            'tg': False,
-            'rm_critique': False,
-            'rm_feedback': False,
-            'hp_h2m':False
-        }
-    elif dataset_name == 'hp_h2m':
-        return {
-            'max_new_tokens': 300,
-            'planning': False,
-            'tg': False,
-            'rm_critique': False,
-            'rm_feedback': False,
-            'hp_h2m':True
-        }
-    elif dataset_name == 'hp_m2l':
-        return {
-            'max_new_tokens': 300,
-            'planning': False,
-            'tg': False,
-            'rm_critique': False,
-            'rm_feedback': False,
-            'hp_h2m':False,
-            'hp_m2l':True
-        }
-    else:
-        return {
-            'max_new_tokens': 30,
-            'planning': False,
-            'tg': False,
-            'rm_critique': False,
-            'rm_feedback': False,
-            'hp_h2m': False
-        }
-
-def get_dataset_name(annotation_path):
-    folder_name = os.path.dirname(annotation_path).split('/')[-1]
-    file_name = annotation_path.split('/')[-1]
-    dataset_name = folder_name
-    if folder_name == 'rm':
-        if 'critique' in file_name:
-            dataset_name = 'rm_critique'
-        elif 'feedback' in file_name:
-            dataset_name = 'rm_feedback'
-        else:
-            raise ValueError("Invalid rm file name.")
-    elif folder_name == 'hp':
-        if 'h2m' in file_name:
-            dataset_name = 'hp_h2m'
-        elif 'm2l' in file_name:
-            dataset_name = 'hp_m2l'
-        else:
-            raise ValueError("Invalid hp file name.")
-    elif folder_name == 'vqa':
-        dataset_name = 'vqa'
-    elif folder_name == 'tg':
-        dataset_name = 'tg'
-    elif folder_name == 'og':
-        dataset_name = 'og'
-    elif folder_name == 'fg':
-        dataset_name = 'fg'
-    else:
-        raise ValueError("Invalid file name.")
-    return dataset_name
-
-def get_video_path(dataset_name):
-    # video_folder = "/apdcephfs_cq10/share_1150325/csj/videgothink/goalstep_val_clean"
-    video_folder = "/apdcephfs_cq10/share_1150325/csj/videgothink/goalstep_val_hp_video"
-    if 'vqa' in dataset_name:
-        video_folder = video_folder
-    elif 'rm' in dataset_name:
-        video_folder = video_folder + "/rm"
-    elif 'hp' in dataset_name:
-        video_folder = video_folder
-    elif 'tg' in dataset_name:
-        video_folder = video_folder + "/tg"
-    elif 'og' in dataset_name:
-        video_folder = video_folder + "/og"
-    elif 'fg' in dataset_name:
-        video_folder = video_folder + "/fg"
-    else:
-        raise ValueError("Invalid video path.")
-    return video_folder
-
+    # 基础参数模板
+    base_args = {'max_new_tokens': 300}
+    # 定义不同数据集的特定配置
+    dataset_configs = {
+        "tg": {'max_new_tokens': 128, 'tg': True},
+        "og": {'max_new_tokens': 128, 'og': True},
+        "fg": {'max_new_tokens': 128, 'fg': True},
+        "rm_critique": {'max_new_tokens': 128, 'rm_critique': True},
+        "rm_feedback": {'max_new_tokens': 128, 'rm_feedback': True},
+        "assistance": {'max_new_tokens': 300, 'planning': True},
+        "navigation": {'max_new_tokens': 300, 'planning': True},
+        "hp_h2m": {'max_new_tokens': 300, 'hp_h2m': True},
+        "hp_m2l": {'max_new_tokens': 300, 'hp_m2l': True},
+        "vqa":{'max_new_tokens': 300}
+    }
+    # 获取并合并配置
+    config = dataset_configs.get(dataset_name)
+    if config is None:
+        # 如果没有找到匹配的配置，警告
+        warnings.warn(f"Warning: No specific configuration found for dataset '{dataset_name}'. Using base configuration.")
+        config = base_args
+    return {**base_args, **config}
 
 def main(args):
     os.environ['CUDA_VISIBLE_DEVICES'] = str(args.device)
     model_names = args.model_name.split(',')  
-
-    # primary_abilities = args.primary_ability.split(',')
-    # if primary_abilities[0] == 'all':
-    #     primary_abilities = abilities.keys()
-    # secondary_abilities = args.secondary_ability.split(',')
     time = datetime.datetime.now().strftime("%m%d-%H%M")
-    # path: /${dataset}/annotations.json 
-    # if "hp_m2l" in args.answer_path:
-    #     dataset_name = 'hp_m2l'
-    # elif "hp_h2m" in args.answer_path:
-    #     dataset_name = 'hp_h2m'
-    # else:
-    #     dataset_name = 'Other'
     dataset_name = args.dataset_name
 
     print(f"dataset name: {dataset_name}")
@@ -196,8 +108,6 @@ def main(args):
             batch_size = args.batch_size
         device = torch.device('cuda') if torch.cuda.is_available() else "cpu"
         model = get_model(model_name, device=device)
-        # time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-        # answer_path = f"{args.answer_path}/{args.model_name}"
         
         dataset = load_dataset(args)
         dataloader = DataLoader(dataset, batch_size=1, collate_fn=lambda batch: {key: [dict[key] for dict in batch] for key in batch[0]})
@@ -208,17 +118,16 @@ def main(args):
         q_id = 0
         for batch in tqdm(dataloader, desc=f"Running inference: {model_name} on  {dataset_name}"):
             questions = batch['question']
-            # print(questions)
-            images = batch['images']
-            if args.data_mode == "VIDEO":
-                video_path = get_video_path(dataset_name)
-                print(f"video path: {video_path}")
-                images = [ video_path + "/" + x for x in batch['video_path']]
 
-            print(f"args.data_mode:{args.data_mode} , dataset_name: {dataset_name}, Images:{images}")     
+            input_media = {
+                'image': batch.get('images', None),
+                'video': batch.get('video', None)
+            }.get(args.data_mode, None) # choose the right type of data mode
+            print(f"args.data_mode:{args.data_mode} , dataset_name: {dataset_name}, media: {input_media}")  
+
             if args.batch_size == 1:
                 try:
-                    output = model.generate(images[0], questions[0], **get_generation_args(dataset_name))
+                    output = model.generate(input_media[0], questions[0], **get_generation_args(dataset_name))
                     print(output)
                     outputs = [output]
                 except Exception as e:
@@ -227,7 +136,7 @@ def main(args):
                     print(f"Error: {e}")
                     outputs = ['']
             else:
-                outputs = model.batch_generate(images, questions, **get_generation_args(dataset_name))
+                outputs = model.batch_generate(input_media, questions, **get_generation_args(dataset_name))
             for i, (question, answer, pred) in enumerate(zip(batch['question'], batch['answer'], outputs)):
                 # answer_dict={'question': questions, 'prediction': pred,
                 # 'gt_answers': answer}
